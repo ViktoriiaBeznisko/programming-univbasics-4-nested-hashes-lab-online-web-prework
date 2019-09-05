4cde946e130296ade614a39b553d87f005ad77c2
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-05 05:34:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,7 +125,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:denis_ritchie][:languages]["Assembly"] = ["Assembly"]
+programmer_hash[:denis_ritchie][:languages]["Assembly"] = "Assembly"
 p adding_to_dennis
 end
 #it "operates on the programmer_hash and adds 'Assembly' to Dennis Ritchie's languages, returning the newly-added-to-hash" do
